62b75cc7c60d6c0543d4e5eec560e79251c31716
yakub43 <yakubakku43@gmail.com>
2024-03-20 22:55:55 +0530

    Added code for user defined api error response

--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -1,17 +1,27 @@
 package net.javaguides.springboot.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class UserDto {
     private Long id;
+    // User firstName should not be null or empty
+    @NotEmpty(message = "User firstName should not be null or empty")
     private String firstName;
+
+    // User last name should not be null or empty
+    @NotEmpty(message = "User last name should not be null or empty")
     private String lastName;
+
+    // email should be valid
+    // user email should not be null or empty
+    @NotEmpty(message = "User email should not be null or empty")
+    @Email
     private String email;
 }
--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/exception/ErrorDetails.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/exception/ErrorDetails.java
@@ -1,9 +1,6 @@
 package net.javaguides.springboot.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +8,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class ErrorDetails {
     private LocalDateTime timestamp;
     private String message;
