44ac69708224c4e872c608cbe967456f4078e44c
yakub43 <yakubakku43@gmail.com>
2024-03-25 13:12:36 +0530

    Added code for circuit breaker

--- Microservices_Project/employee-service/src/main/java/com/net/employeeservice/service/impl/EmployeeServiceImpl.java
+++ Microservices_Project/employee-service/src/main/java/com/net/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -7,6 +7,7 @@ import com.net.employeeservice.entity.Employee;
 import com.net.employeeservice.repo.EmployeeRepository;
 import com.net.employeeservice.service.APIClient;
 import com.net.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -43,12 +44,37 @@ public class EmployeeServiceImpl implements EmployeeService {
         return savedEmployeedto;
     }
     @Override
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     public APIResponseDTO getEmployeeById(Long employeeId) {
 
         Employee employee = employeeRepository.findById(employeeId).get();
 
        /* ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);*/
         DepartmentDto responseEntity = apiClient.getDepartment(employee.getDepartmentCode());
+
+        EmployeeDto savedEmployeedto = new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getDepartmentCode()
+        );
+
+        APIResponseDTO apiResponseDTO = new APIResponseDTO();
+
+        apiResponseDTO.setEmployeeDto(savedEmployeedto);
+        apiResponseDTO.setDepartmentDto(responseEntity);
+
+        return apiResponseDTO;
+    }
+    public APIResponseDTO getDefaultDepartment(Long employeeId, Exception exception){
+
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto responseEntity = new DepartmentDto();
+        responseEntity.setDepartmentName("R&D Department");
+        responseEntity.setDepartmentCode("RD001");
+        responseEntity.setDepartmentDescription("Research and Development Department");
 
         EmployeeDto savedEmployeedto = new EmployeeDto(
                 employee.getId(),
