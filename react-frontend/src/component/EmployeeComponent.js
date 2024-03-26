e74064161857db8574e7fbe3d2478d64aa945eec
yakub43 <yakubakku43@gmail.com>
2024-03-26 19:28:58 +0530

    Create class EmployeeService

---
+++ react-frontend/src/component/EmployeeComponent.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+class EmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            employee: {},
+            department: {},
+            organization: {}
+        }
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
